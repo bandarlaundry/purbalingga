e4a6fb48a649cec4af308cb85cc8ad01c94f2847
Bandar Laundry <bandarlaundry@gmail.com>
2025-03-12 08:50:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.link').forEach(link => {
+    link.addEventListener('click', () => {
+        console.log(`Clicked on ${link.href}`);
+        // You can add more functionality here, like tracking clicks or opening links in new tabs.
+    });
+});
